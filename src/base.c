ec1249bc9609683a6d98007f0a9ad8aa31adadb6
Roger Orchard <github@toniroger.me.uk>
2013-10-05 17:15:24 +0100

    Test

---
+++ src/base.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : base.c
+ Author      : Roger
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
