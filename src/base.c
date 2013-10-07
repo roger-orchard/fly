0c9296984cfc6a9310879fcb1caf0fa4cf64665e
Roger Orchard <github@toniroger.me.uk>
2013-10-07 10:53:43 +0100

    test

--- src/base.c
+++ src/base.c
@@ -11,7 +11,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main(void)
+{
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	// Start OS
+
+	// find sensors
+
+	// Find motors
+
+	// test motors
+
+	// get location
+
+	// get waypoints
+
+	// start loop waypoints
+
+	// set high
+	// set dir
+	// check status
+    // check update
+
+	// end loop
+
+	// Check home
+
 	return EXIT_SUCCESS;
 }
